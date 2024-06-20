48ee109a20e9035bbb4ecaef5542e6e1a5c6598c
Lukasyaaa <lukashmaxim1@gmail.com>
2024-06-20 10:47:31 +0300

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, MouseEvent, useRef, useEffect } from "react";
+import Header from "./components/Header";
+import "./styles/App.scss"
 
 export const App : FC = () =>{
+    let header = useRef<HTMLElement>(null);
+
+    const appearMenu = (e : MouseEvent<HTMLButtonElement>) : void =>{
+        if(header.current){
+            e.currentTarget.classList.toggle("_active");
+            header.current.classList.toggle("_active");
+        }
+    }
+
+    const changeHeader = (e : Event) : void =>{
+        if(header.current){
+            if(window.scrollY > 0){
+                header.current.classList.add("_scroll");
+            }else{
+                header.current.classList.remove("_scroll");
+            }
+        }
+    }
+
+    useEffect(() =>{
+        window.addEventListener('scroll', changeHeader)
+        return () => {
+            window.removeEventListener('scroll', changeHeader);
+        };
+    }, [])
+
     return(
         <Fragment>
-            
+            <Header appearMenu={appearMenu} ref={header} />
         </Fragment>
     )
 }
